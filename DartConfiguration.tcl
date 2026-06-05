093198d7fa522a952a5ba03f24e5034226f9baf0
swords02 <owengerke@icloud.com>
2026-06-05 11:47:30 -0400

    Update 15.3 branding and languages

--- DartConfiguration.tcl
+++ DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: /src/build
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: 5c934bf76cdf
+Site: 6025dd3b44e5
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Emscripten-em++
